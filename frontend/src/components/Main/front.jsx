aeee286c0f9c479608f07e27c9f775f680776a45
Raghavendra Kotnis <raghavendra_kotnis05@icloud.com>
2024-04-06 21:04:23 +0530

    raghav

---
+++ frontend/src/components/Main/front.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./header";
+import SearchBar from "./searchbar";
+import Scroll from "./scroll";
+
+function Front(){
+    return (
+        <div>
+    <Header />
+    <SearchBar />
+    <Scroll />
+</div>
+    );
+}
+export default Front;
